91b9d8156a489416f51de2c0bed83891c6de3370
Thuan Vu <vugiathuan123@gmail.com>
2023-12-12 21:36:41 -0800

    Adding Batch Norm and CrossEntropyLoss

--- BatchNorm.py
+++ BatchNorm.py
@@ -1,9 +1,10 @@
 from OptTensor import OptTensor
 import numpy as np
 
+
 class BatchNorm:
     def __init__(self):
-        self.gamma = None 
+        self.gamma = None
         self.gamma_grad = None
         self.beta = None
         self.beta_grad = None
@@ -15,17 +16,19 @@ class BatchNorm:
         norm = (data.data - self.mean_moving) / self.mean_std
         return OptTensor(norm, children=data, backward_func='BatchNorm')
 
-    def __call__(self, data:OptTensor):
+    def __call__(self, data: OptTensor):
         if not self.gamma:
             self.gamma = np.ones(data.shape[1])
         if not self.beta:
             self.beta = np.zeros(data.shape[1])
         norm = data.norm()
-        norm_out = norm(gamma) + self.beta
+        norm_out = norm(self.gamma) + self.beta
         out = OptTensor(norm_out, children=data, backward_func='BatchNorm')
         data.out = out
-        self.meaning_moving = self.alpha + (1-self.alpha)*data.data.mean(axis=0)
+        self.meaning_moving = self.alpha + \
+            (1-self.alpha)*data.data.mean(axis=0)
         self.mean_std = self.alpha + (1-self.alpha)*data.data.std(axis=0)
+
         def backward():
             # y = v*X_mean + b
             # dy/dv = X_mean
@@ -34,8 +37,8 @@ class BatchNorm:
             # gamma_grad = dL/dv = dL/dy * dy/dv
             # gamma_grad = dL/dv = dL/dy * X_mean
             # bias_grad = dL/db = dL/dy * dy/db
-            # bias_grad = dL/db = dL/dy * 
+            # bias_grad = dL/db = dL/dy *
             # data.grad: dL/dx = dL/dy * dy/dx
-            data.grad = out.grad # dL/dy
+            data.grad = out.grad  # dL/dy
         out._backward = backward
         return out
